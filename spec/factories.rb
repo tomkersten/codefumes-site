da92689ecd359189c833f4031c928f6ec0f79127
Tom Kersten <tkersten@obtiva.com>
2009-04-15 21:45:44 -0500

    Added factories for personas

--- spec/factories.rb
+++ spec/factories.rb
@@ -12,3 +12,24 @@ Factory.define :typical_user, :class => User do |f|
   f.password 'password'
   f.password_confirmation 'password'
 end
+
+Factory.define :sam, :class => User do |f|
+  f.email "sam@flomericabank.com"
+  f.login "sam.surfer"
+  f.password 'password'
+  f.password_confirmation 'password'
+end
+
+Factory.define :oscar, :class => User do |f|
+  f.email "oscar@haxit.com"
+  f.login "oscar.opensource"
+  f.password 'password'
+  f.password_confirmation 'password'
+end
+
+Factory.define :dora, :class => User do |f|
+  f.email "dora@midco.com"
+  f.login "dora.developer"
+  f.password 'password'
+  f.password_confirmation 'password'
+end
