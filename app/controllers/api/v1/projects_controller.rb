14cca08ced7e2d4a851ca710057870ef96962053
Tom Kersten <tkersten@obtiva.com>
2009-05-15 21:41:14 -0500

    Finished out CRUD support for project api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ProjectsController < ApplicationController
   def index
     @projects = Project.all
-
     respond_to do |format|
       format.xml {}
     end
@@ -16,12 +15,31 @@ class Api::V1::ProjectsController < ApplicationController
 
   def create
     @project = Project.new(params[:project])
-
     respond_to do |format|
       if @project.save
         format.xml {render :status => :created, :location => api_v1_project_url(:xml, @project)}
       else
         format.xml {render :xml => @project.errors, :status => :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @project = Project.find_by_id(params[:id])
+    @project.destroy unless @project.nil?
+    respond_to do |format|
+      format.xml {render :text => nil }
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      if @project.update_attributes(params[:project])
+        format.xml {render :location => api_v1_project_url(:xml, @project)}
+      else
+        format.xml {render :status => :unprocessable_entity, :location => api_v1_project_url(:xml, @project)}
       end
     end
   end
--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -27,7 +27,7 @@ describe Api::V1::ProjectsController do
     end
   end
 
-  describe "POST to create" do
+  describe "a POST to create" do
     describe "with valid params" do
       before(:each) do
         @params = Project.plan
@@ -59,6 +59,73 @@ describe Api::V1::ProjectsController do
     it "assigns the specified project for the view template" do
       get :show, :id => @project.id
       assigns[:project].should == @project
+    end
+  end
+
+  describe "a DELETE to destroy" do
+    before(:each) do
+      @project = Project.make
+    end
+
+    def perform_request
+      delete :destroy, :id => @project
+    end
+
+    it "deletes the specified project" do
+      perform_request
+      Project.find_by_id(@project.id).should be_nil
+    end
+
+    it "returns a response code of 200 OK" do
+      perform_request
+      response.should be_success
+    end
+
+    context "when the resource does not exist" do
+      before(:each) do
+        Project.destroy_all
+      end
+
+      it "returns a response code of 200 OK" do
+        perform_request
+        response.should be_success
+      end
+    end
+  end
+
+  describe "a PUT to update" do
+    before(:each) do
+      @project = Project.make
+      @update_params = Project.plan
+    end
+
+    def perform_request
+      put :update, :id => @project.id, :project => @update_params
+    end
+
+    context "with valid parameters" do
+      it "returns a response code of 200 OK" do
+        perform_request
+        response.should be_success
+      end
+
+      it "updates the specified project with the supplied values" do
+        perform_request
+        @project.reload
+        @project.name.should == @update_params[:name]
+      end
+    end
+
+    context "with invalid parameters" do
+      before(:each) do
+        @project2 = Project.make
+        @update_params = {:public_key => @project2.public_key}
+      end
+
+      it "returns a response code of 422 Unprocessable Entity" do
+        perform_request
+        response.status.should == "422 Unprocessable Entity"
+      end
     end
   end
 end
