8d075fcf92c6a641f3fe0926e0c00d474e2365c2
Tom Kersten <tom.kersten@cosyntech.com>
2009-08-22 11:20:15 -0500

    Fixed failing JS test & added animation fluff ;-)

--- spec/javascripts/initializer_spec.js
+++ spec/javascripts/initializer_spec.js
@@ -4,7 +4,7 @@ require("../../public/javascripts/initializer.js");
 Screw.Unit(function(){
   describe("Initializer", function(){
     it("should expand contents of list item when header is clicked", function(){
-      $('ul.commits').trigger('click')
+      $('ul.commits li h2').trigger('click')
       expect($("ul.commits li ul").css("display")).to(equal,"block");
     });
   });
