a97711ab0924221cfed84802a44065f25a8d33e9
Matt Loseke <mloseke@gmail.com>
2009-09-10 21:11:45 -0500

    add style for cursor on hover of commit h2s
    just a one-off for now, could be generalized at some point

--- public/javascripts/initializer.js
+++ public/javascripts/initializer.js
@@ -1,5 +1,16 @@
 $(function(){
-  $('ul.commits li ul').css("display","none")
+  $('ul.commits li ul').css("display", "none")
+  $('ul.commits li h2').hover(
+    function(){
+      $(this).css({
+        'cursor' : 'hand'
+      });
+    }, function(){
+      $(this).css({
+        'cursor' : 'pointer'
+      });
+    }
+  );
   $('ul.commits li h2').toggle(
   function(){
     $(this).parent().find('ul').slideDown('fast');
