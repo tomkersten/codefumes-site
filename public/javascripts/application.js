8a07cf10a0c563e5667caecd5cb3c6d1f51fcac5
Matt Loseke <mloseke@gmail.com>
2009-10-05 21:48:45 -0500

    update styles for textfield and labels, add toggling between visualizations on project page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,11 @@
+$(function(){
 
+  $('input').focus(function(){
+    $(this).parent().prev('div').addClass('focus');
+  });
+
+  $('input').blur(function(){
+    $(this).parent().prev('div').removeClass('focus');
+  });
+  
+});
--- spec/javascripts/initializer_spec.js
+++ spec/javascripts/initializer_spec.js
@@ -1,15 +1,30 @@
 require("spec_helper.js");
 require("../../public/javascripts/initializer.js");
 
-//Screw.Unit(function(){
-//  describe("Initializer", function(){
-//    it("should expand contents of list item when header is clicked", function(){
-//    console.debug("foo");
-//      $('ul.commits li h2').trigger('click');
-//      //expect($("ul.commits li ul").css("display")).to(equal,"block");
-//      
-//      expect($("ul.commits li ul").css("display")).to(equal,"block");
-//    });
-//  });
-//});
+Screw.Unit(function(){
 
+  describe("Initializer", function(){
+
+    it("should not initially show the contents of list item", function(){
+
+      $("ul.commits li ul").each(function(){
+        expect($(this).is(':visible')).to(equal, false);
+      });
+    
+    });
+
+    it("should expand contents of list items when respective header is clicked", function(){
+
+      $('ul.commits li h2').each(function(){
+        $(this).trigger('click');
+      });
+
+      $("ul.commits li ul").each(function(){
+        expect($(this).is(':visible')).to(equal, true);
+      });
+  
+    });
+  
+  });
+
+});
