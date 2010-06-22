ba24fbd01b0df800146cb581565212a225495432
Roy Kolak <roy.kolak@gmail.com>
2010-06-22 14:36:28 -0500

    Jasmine Jazz runner includes files from jasmine config

--- spec/javascripts/support/run_specs_with_jazz_money.rb
+++ spec/javascripts/support/run_specs_with_jazz_money.rb
@@ -1,27 +1,24 @@
 require 'rubygems'
 require 'jazz_money'
+require 'yaml'
 
-javascript_files = [
-  'public/javascripts/frameworks/jquery.js',
-  'public/javascripts/frameworks/jquery.dates.js',
-  'public/javascripts/frameworks/raphael.js',
-  'public/javascripts/monkey_patches.js',
-  'public/javascripts/visualizations/commits_over_time/commits_over_time.js',
-  'public/javascripts/visualizations/commits_over_time/grid.js',
-  'public/javascripts/visualizations/commits_over_time/svgmodal.js',
-  'public/javascripts/visualizations/linegraph.js',
-  'public/javascripts/visualizations/visualization_error.js',
-  'public/javascripts/expand_collapse.js'
-]
+code = []
+specs = []
 
-jasmine_spec_files = [
-  'spec/javascripts/monkey_patches_spec.js',
-  'spec/javascripts/linegraph_spec.js',
-  'spec/javascripts/commits_over_time_spec.js',
-  'spec/javascripts/grid_spec.js',
-  'spec/javascripts/svgmodal_spec.js',
-  'spec/javascripts/visualization_error_spec.js',
-  'spec/javascripts/expand_collapse_spec.js'
-]
+config = YAML::load_stream(File.open('spec/javascripts/support/jasmine.yml')).documents[0]
 
-JazzMoney::Runner.new(javascript_files, jasmine_spec_files).call
+
+config['src_files'].each do |path|
+  Dir["#{config['src_dir']}#{path}"].each do |file| 
+    code << file
+  end
+end
+
+config['spec_files'].each do |path|
+  Dir["#{config['spec_dir']}#{path}"].each do |file| 
+    specs << file
+  end
+end
+
+
+JazzMoney::Runner.new(code, specs).call
