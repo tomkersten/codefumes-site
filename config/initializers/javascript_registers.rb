ee39178d0833def2b9b98c4ba2714f6aa82997ac
Roy Kolak <roy.kolak@gmail.com>
2010-06-24 18:30:23 -0500

    add less framework and new empty css

--- config/initializers/javascript_registers.rb
+++ config/initializers/javascript_registers.rb
@@ -1,4 +1,4 @@
-javascript_frameworks = ["frameworks/jquery", "frameworks/jquery.dates", "frameworks/raphael", "monkey_patches"]
+javascript_frameworks = ["frameworks/jquery", "frameworks/jquery.dates", "frameworks/raphael", "frameworks/less", "monkey_patches"]
 javascript_visualizations = ["visualizations/visualization_error", "visualizations/linegraph", "visualizations/commits_over_time/commits_over_time", "visualizations/commits_over_time/grid", "visualizations/commits_over_time/svgmodal"]
 javascript_interface = ["expand_collapse", "initializer"]
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :frameworks => javascript_frameworks
