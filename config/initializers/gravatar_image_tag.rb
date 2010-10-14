339f649fe816ec0f33b1c8eeb2f953e7b721bd01
Tom Kersten <tom.kersten@codefumes.com>
2010-10-14 08:09:49 -0500

    Added gravatar_image_tag gem

---
+++ config/initializers/gravatar_image_tag.rb
@@ -0,0 +1,7 @@
+GravatarImageTag.configure do |config|
+  config.default_image = nil
+  config.filetype      = nil
+  config.rating        = nil
+  config.size          = 65
+  config.secure        = false
+end
